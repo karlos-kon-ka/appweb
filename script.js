edf3db8a84a8affda6e42daffc983d7b722a106a
Karlos <143749395+karlos-kon-ka@users.noreply.github.com>
2023-11-11 20:31:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function convertirADias() {
-    var horas = document.getElementById("horas").value;
+    var horas = parseFloat(document.getElementById("horas").value) || 0;
     var dias = horas / 24;
-    document.getElementById("dias").value = dias;
+    document.getElementById("dias").value = dias.toFixed(2);
 }
 
 function convertirAHoras() {
-    var dias = document.getElementById("dias").value;
+    var dias = parseFloat(document.getElementById("dias").value) || 0;
     var horas = dias * 24;
-    document.getElementById("horas").value = horas;
+    document.getElementById("horas").value = horas.toFixed(2);
 }
+
+// Agregar event listeners para actualización dinámica
+document.getElementById("horas").addEventListener("input", convertirADias);
+document.getElementById("dias").addEventListener("input", convertirAHoras);
